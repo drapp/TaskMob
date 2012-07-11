f9f5f55325de1957f85e0740c6baf18b3d362362
Douglas Rapp <drapp@stackmob.com>
2012-07-11 13:19:55 -0700

    Update to android sdk 0.5.0

--- stackmob-android-v0.5.0/src/com/stackmob/android/sdk/common/StackMobCommon.java
+++ stackmob-android-v0.5.0/src/com/stackmob/android/sdk/common/StackMobCommon.java
@@ -22,10 +22,12 @@ import com.stackmob.android.sdk.callback.*;
 import com.stackmob.sdk.callback.StackMobRedirectedCallback;
 import com.stackmob.sdk.api.StackMob;
 import com.stackmob.sdk.api.StackMobRequest;
+import com.stackmob.sdk.api.StackMob.OAuthVersion;
 
 import java.util.Map;
 
 public class StackMobCommon {
+	public static OAuthVersion OAUTH_VERSION = OAuthVersion.One;
 	public static String API_KEY = "YOUR_API_KEY_HERE";
 	public static String API_SECRET = "YOUR_API_SECRET_HERE";
 	public static String USER_OBJECT_NAME = "YOUR_USER_OBJECT_NAME_HERE";
@@ -39,6 +41,8 @@ public class StackMobCommon {
 	
 	public static String FACEBOOK_APP_ID = "YOUR_FACEBOOK_APP_ID_HERE";
 	
+	public static boolean LOGGING_ENABLED = false;
+	
 	public static StackMobTwitterCallback TwitterCallback = null;
 	public static StackMobFacebookCallback FacebookCallback = null;
 
@@ -51,8 +55,12 @@ public class StackMobCommon {
 	};
 	
 	public static void init(Context c) {
-		StackMob.setStackMob(new StackMob(API_KEY, API_SECRET, USER_OBJECT_NAME, API_VERSION, API_URL_FORMAT, PUSH_API_URL_FORMAT, redirectedCallback));
+		StackMob.setStackMob(new StackMob(OAUTH_VERSION, API_KEY, API_SECRET, USER_OBJECT_NAME, API_VERSION, API_URL_FORMAT, PUSH_API_URL_FORMAT, redirectedCallback));
+		StackMob.getStackMob().setSession(new StackMobAndroidSession(c, StackMob.getStackMob().getSession()));
+		StackMob.getStackMob().getSession().setOAuthVersion(OAUTH_VERSION); //The copy ctor needs to be fixed
 		StackMob.setUserAgentName("Android");
+		StackMob.setLogger(new StackMobAndroidLogger());
+		StackMob.getLogger().setLogging(LOGGING_ENABLED);
 		StackMobRequest.setCookieStore(new StackMobAndroidCookieStore(c));
 		initialized = true;
 	}
